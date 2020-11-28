bba758f204d22ad1f06b9c02f0cf8131516f360d
Leo Maroni <git@em0lar.de>
2020-11-28 19:49:37 +0100

    Add error messages to login/register form, some language stuff

--- alpakanews/settings.py
+++ alpakanews/settings.py
@@ -123,9 +123,9 @@ AUTHENTICATION_BACKEND = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'de-de'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
