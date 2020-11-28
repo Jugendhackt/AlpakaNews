1bf78a80c028b56f9f81122e6aac8f544b509155
Leo Maroni <git@em0lar.de>
2020-11-28 14:54:56 +0100

    Add templates folder

--- alpakanews/settings.py
+++ alpakanews/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'alpakanews.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            "alpakanews/templates/"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
