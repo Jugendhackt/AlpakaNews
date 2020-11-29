e14260e34b76d7c01777bab649c989383e2fc963
Leo Maroni <git@em0lar.de>
2020-11-29 12:42:33 +0100

    Add created at timestamp for comment

--- core/models.py
+++ core/models.py
@@ -66,3 +66,4 @@ class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
     text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
